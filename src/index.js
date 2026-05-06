1a7345db7720688b1ca3016ea64eb445d957582f
Matthew <mpolisch@uoguelph.ca>
2026-05-06 10:39:49 -0400

    feat: all routes in entrypoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 
 const warehousesRouter = require('./routes/warehouses');
+const partsRouter = require('./routes/parts')
+const locationsRouter = require('./routes/locations');
+const inventoryRouter = require('./routes/inventory');
+const auditRouter = require('./routes/audit');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
@@ -10,6 +14,10 @@ const app = express();
 app.use(express.json());
 
 app.use('/warehouses', warehousesRouter);
+app.use('/parts', partsRouter)
+app.use('/locations', locationsRouter);
+app.use('/inventory', inventoryRouter);
+app.use('/audit', auditRouter);
 
 app.use(errorHandler);
 
